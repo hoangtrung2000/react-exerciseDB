9f90da4b441daafb51cc288cdbaa080227d92b20
187PM14027 Nguyen Hoang Trung K24T <hoangtrung7762@gmail.com>
2022-06-18 00:34:50 +0700

    GUI #1

---
+++ src/components/HorizontalScrollBar.js
@@ -0,0 +1,53 @@
+import React, { useContext } from "react";
+import PropTypes from "prop-types";
+import { Box, Typography } from "@mui/material";
+import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
+
+import LeftArrowIcon from "../assets/icons/left-arrow.png";
+import RightArrowIcon from "../assets/icons/right-arrow.png";
+import BodyParts from "./BodyParts";
+
+const LeftArrow = () => {
+  const { scrollPrev } = useContext(VisibilityContext);
+  return (
+    <Typography onClick={() => scrollPrev()} className="right-arrow">
+      <img src={LeftArrowIcon} alt="left arrow" />
+    </Typography>
+  );
+};
+
+const RightArrow = () => {
+  const { scrollNext } = useContext(VisibilityContext); // use porps of VisibilityContext provided
+  return (
+    <Typography onClick={() => scrollNext()} className="left-arrow">
+      <img src={RightArrowIcon} alt="right arrow" />
+    </Typography>
+  );
+};
+
+function HorizontalScrollBar({ data, bodyPart, setBodyPart }) {
+  return (
+    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
+      {data.map((item) => (
+        <Box
+          key={item.id || item}
+          itemId={item.id || item}
+          title={item.id || item}
+          m="0 40px"
+        >
+          <BodyParts
+            item={item}
+            bodyPart={bodyPart}
+            setBodyPart={setBodyPart}
+          />
+        </Box>
+      ))}
+    </ScrollMenu>
+  );
+}
+
+HorizontalScrollBar.propTypes = {
+  data: PropTypes.array.isRequired,
+};
+
+export default HorizontalScrollBar;
