1cb61d4a5e042606c4ebe1c098bb90a391be32af
187PM14027 Nguyen Hoang Trung K24T <hoangtrung7762@gmail.com>
2022-06-26 00:51:30 +0700

    'Deloy'

--- src/components/BMICalculator.js
+++ src/components/BMICalculator.js
@@ -1,7 +1,15 @@
 import React, { useState } from "react";
-import { TextField, Typography, Button, Box, Stack } from "@mui/material";
+import {
+  TextField,
+  Typography,
+  Button,
+  Box,
+  Stack,
+  autocompleteClasses,
+} from "@mui/material";
 
 import { fitnessCalculatorOptions, fetchData } from "../utils/fetchData";
+import { maxWidth, width } from "@mui/system";
 
 function BMiCalculator() {
   const [age, setAge] = useState("");
@@ -23,44 +31,79 @@ function BMiCalculator() {
     }
   };
   return (
-    <Box mt="500px">
-      <Stack direction="row" gap="20px">
-        <TextField
-          sx={{ width: "200px" }}
-          value={age}
-          onChange={(e) => setAge(e.target.value)}
-          id="outlined-basic"
-          label="Age"
-          variant="outlined"
-          type="number"
-          InputProps={{ inputProps: { min: 0, max: 80 } }}
-        />
-        <TextField
-          sx={{ width: "200px" }}
-          value={weight}
-          onChange={(e) => setWeight(e.target.value)}
-          id="outlined-basic"
-          label="Weight"
-          variant="outlined"
-          type="number"
-          InputProps={{ inputProps: { min: 40, max: 160 } }}
-        />
-        <TextField
-          sx={{ width: "200px" }}
-          value={height}
-          onChange={(e) => setHeight(e.target.value)}
-          id="outlined-basic"
-          label="Height"
-          variant="outlined"
-          type="number"
-          InputProps={{ inputProps: { min: 130, max: 230 } }}
-        />
-      </Stack>
-      <Button onClick={fetchBmiData} variant="contained">
-        Calculator
-      </Button>
-      <Typography variant="h5">Ket qua: {result.bmi}</Typography>
-    </Box>
+    <Stack
+      mx="auto"
+      sx={{ maxWidth: "800px", padding: { xs: "0 10px", md: "0" } }}
+      direction="column"
+      gap="20px"
+    >
+      <Typography variant="h3" textAlign="center" pt="20px">
+        Body Mass Index Calculator
+      </Typography>
+      <Box
+        sx={{
+          border: "2px solid #ff262557",
+          borderRadius: "10px",
+          width: "100%",
+        }}
+      >
+        <Stack
+          justifyContent="center"
+          alignItems="center"
+          direction="column"
+          gap="20px"
+          py="20px"
+        >
+          <TextField
+            sx={{ width: "200px" }}
+            value={age}
+            onChange={(e) => setAge(e.target.value)}
+            id="outlined-basic"
+            label="Age"
+            variant="outlined"
+            type="number"
+            InputProps={{ inputProps: { min: 0, max: 80 } }}
+          />
+          <TextField
+            sx={{ width: "200px" }}
+            value={weight}
+            onChange={(e) => setWeight(e.target.value)}
+            id="outlined-basic"
+            label="Weight"
+            variant="outlined"
+            type="number"
+            InputProps={{ inputProps: { min: 40, max: 160 } }}
+          />
+          <TextField
+            sx={{ width: "200px" }}
+            value={height}
+            onChange={(e) => setHeight(e.target.value)}
+            id="outlined-basic"
+            label="Height"
+            variant="outlined"
+            type="number"
+            InputProps={{ inputProps: { min: 130, max: 230 } }}
+          />
+          <Button
+            className="search-btn"
+            onClick={fetchBmiData}
+            sx={{
+              backgroundColor: "#ff2625",
+              color: "#fff",
+              border: "1px solid transparent",
+            }}
+          >
+            Calculator
+          </Button>
+        </Stack>
+      </Box>
+      <Typography variant="h4" textAlign="center" color="#ff2625">
+        {result.health}
+      </Typography>
+      <Typography variant="h5" textAlign="center">
+        BMI: {result.bmi}
+      </Typography>
+    </Stack>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,56 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Stack } from "@mui/material";
 
 import Logo from "../assets/images/Logo.png";
 
 function Navbar() {
+  const [isMobile, setIsMobile] = useState(true);
+  const handleClick = () => setIsMobile(!isMobile);
   return (
-    <Stack
-      direction="row"
-      justifyContent="space-around"
-      sx={{
-        gap: { sm: "122px", xs: "40px" },
-        mt: { sm: "32px", xs: "20px" },
-        justifyContent: "none",
-        px: "20px",
-      }}
-    >
-      <Link to="/">
-        <img
-          src={Logo}
-          alt="logo"
-          style={{ width: "48px", margin: "0 20px" }}
-        />
-      </Link>
-      <Stack direction="row" gap="40px" fontSize="24px">
-        <Link
-          to="/"
-          style={{
-            textDecoration: "none",
-            color: "#3a1212",
-            borderBottom: "3px solid #ff2625",
-          }}
-        >
-          Home
+    <>
+      <Stack
+        direction="row"
+        justifyContent="space-around"
+        sx={{
+          gap: { sm: "122px", xs: "40px" },
+          mt: { sm: "32px", xs: "20px" },
+          justifyContent: "none",
+          px: "20px",
+        }}
+      >
+        <Link to="/">
+          <img
+            src={Logo}
+            alt="logo"
+            style={{ width: "48px", margin: "0 20px" }}
+          />
         </Link>
-        <a
-          href="#exercises"
-          style={{ textDecoration: "none", color: "#3A1212" }}
+        <Stack
+          direction="row"
+          sx={{ gap: { xs: "20px", md: "40px" } }}
+          fontSize="24px"
         >
-          Exercises
-        </a>
-        <Link
-          to="/bmi"
-          style={{
-            textDecoration: "none",
-            color: "#3a1212",
-          }}
-        >
-          Your BMI
-        </Link>
+          <Link
+            to="/"
+            style={{
+              textDecoration: "none",
+              color: "#3a1212",
+              borderBottom: "3px solid #ff2625",
+            }}
+          >
+            Home
+          </Link>
+          <a
+            href="#exercises"
+            style={{ textDecoration: "none", color: "#3A1212" }}
+          >
+            Exercises
+          </a>
+          <Link
+            to="/bmi"
+            style={{
+              textDecoration: "none",
+              color: "#3a1212",
+            }}
+          >
+            BMI
+          </Link>
+        </Stack>
       </Stack>
-    </Stack>
+    </>
   );
 }
 
